97f4f3f93230c912928bce99d853d7c452070a25
janenjihia <jane.njihia@student.moringaschool.com>
2022-04-26 21:58:20 +0300

    input method to get user input

--- src/main/java/IP1/Encoding.java
+++ src/main/java/IP1/Encoding.java
@@ -5,4 +5,37 @@ public class Encoding {
     private String plaintext = "";
     public Integer cipherKey;
     public String cipherText = "";
+
+    public Encoding() {
+    }
+
+    public void setPlaintext(String plaintext) {
+        this.plaintext = plaintext.toUpperCase();
+    }
+
+    public String getPlaintext() {
+        return this.plaintext;
+    }
+
+    public void userInput() {
+        try {
+            System.out.print("Enter Plaintext: ");
+            InputStreamReader streamReader = new InputStreamReader(System.in);
+            BufferedReader bufferedReader = new BufferedReader(streamReader);
+            String plaintext = bufferedReader.readLine();
+            System.out.print("Enter your Key: ");
+            String cipherKey = bufferedReader.readLine();
+            this.cipherKey = Integer.parseInt(cipherKey);
+            this.setPlaintext(plaintext);
+        } catch (Exception var5) {
+            var5.printStackTrace();
+        }
+
+    }
+
+
+
+
+
+
 }

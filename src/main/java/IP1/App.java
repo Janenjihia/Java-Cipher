92da3c18a94e22700068e84f9b6a1369a5d98257
janenjihia <jane.njihia@student.moringaschool.com>
2022-04-27 05:10:19 +0300

    add alphabets array

--- src/main/java/IP1/App.java
+++ src/main/java/IP1/App.java
@@ -1,4 +1,14 @@
 package IP1;
 
 public class App {
+    public  static  Encoding  encoding =new Encoding();
+    public  static  Decoding  decoding =new Decoding();
+
+    public static void main(String[] args) {
+        encoding.userInput();
+        System.out.println("Your cipher text is" + "" + encoding.cipher());
+
+        decoding.decyrpt(encoding.getPlaintext());
+        System.out.println("Your decrypted phrase is: " + encoding.cipher());
+    }
 }
--- src/main/java/IP1/Encoding.java
+++ src/main/java/IP1/Encoding.java
@@ -1,5 +1,10 @@
 package IP1;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Encoding {
 
     private String plaintext = "";
@@ -17,7 +22,7 @@ public class Encoding {
         return this.plaintext;
     }
 
-    public void userInput() {
+    public String userInput() {
         try {
             System.out.print("Enter Plaintext: ");
             InputStreamReader streamReader = new InputStreamReader(System.in);
@@ -29,6 +34,29 @@ public class Encoding {
             this.setPlaintext(plaintext);
         } catch (Exception var5) {
             var5.printStackTrace();
+        }
+        public String cipher(){
+            String[] alphabets = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+            List<String> myList = new ArrayList(List.of(alphabets));
+            this.setPlaintext(this.plaintext);
+
+            for(int i = 0; i < this.getPlaintext().length(); ++i) {
+                Character myletter = this.getPlaintext().charAt(i);
+                Integer currentIndex = myList.indexOf(myletter.toString());
+                String cipherLetter = "";
+                if (myletter > 'A' && myletter < 'z') {
+                    myletter = (char)(myletter + this.cipherKey);
+                } else if (myletter > 'Z') {
+                    myletter = (char)(myletter + 'A' - 'Z' - 1);
+                    Integer cipherKey = currentIndex + this.cipherKey;
+                    cipherLetter = (String)myList.get(cipherKey);
+                    this.cipherText = this.cipherText + cipherLetter;
+                }
+
+                System.out.println(cipherLetter);
+            }
+
+            return this.cipherText;
         }
 
     }
